09f5402ded20f52e1aaf1e7df800d55085ce9b90
TimothyLear <32465549+TimothyLear@users.noreply.github.com>
2018-10-18 09:06:33 -0400

    Update listen-for-shutdown.py

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
-<other imports>
+
+import RPi.GPIO as GPIO
+import subprocess
 import time 
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.wait_for_edge(3, GPIO.FALLING)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.wait_for_edge(4, GPIO.FALLING)
+
 def shutdown():
-    subprocess.call( <shutdown command> )
+subprocess.call(['shutdown', '-h', 'now'], shell=False)
 
 GPIO.add_event_detect(3, GPIO.FALLING, shutdown)
 GPIO.add_event_detect(4, GPIO.FALLING, shutdown)
